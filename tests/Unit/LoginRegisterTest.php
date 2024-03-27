6a3492987c2d51b972b431a0bfd91a41f54737e5
Lucy Puyenbroek <lucy@luciousdev.nl>
2024-03-27 22:45:24 +0100

    feat: login signup unit tests

---
+++ tests/Unit/LoginRegisterTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\User;
+
+
+class LoginRegisterTest extends TestCase
+{
+    protected $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $response = $this->call('POST', '/register-user', [
+            'full_name' => env('TEST_USER_NAME'),
+            'email' => env('TEST_USER_EMAIL'),
+            'password' => env('TEST_USER_PASSWORD'),
+        ]);
+
+        if ($response->status() === 302) {
+            $this->user = User::where('email', env('TEST_USER_EMAIL'))->first();
+        }
+    }
+
+    public function test_user_verification()
+    {
+//        verify/{email}/{code}
+        // get the code from the registered user
+        $user = User::where('email', env('TEST_USER_EMAIL'))->first();
+        $code = $user->verification_code;
+
+        $response = $this->call('GET', '/verify/' . env('TEST_USER_EMAIL') . '/' . $code);
+
+        $response->assertStatus($response->status(), 302);
+    }
+
+    public function test_user_login()
+    {
+        $response = $this->call('POST', '/login-user', [
+            'email' => env('TEST_USER_EMAIL'),
+            'password' => env('TEST_USER_PASSWORD'),
+        ]);
+
+        $response->assertStatus($response->status(), 302);
+    }
+
+    public function test_delete_user()
+    {
+        if($this->user)
+        {
+            $user = User::where('email', env('TEST_USER_EMAIL'))->first();
+            $user->delete();
+
+            $deletedUser = User::where('email', env('TEST_USER_EMAIL'))->first();
+
+            if($deletedUser)
+            {
+                $this->assertTrue(false);
+            } else
+            {
+                $this->assertTrue(true);
+            }
+        }
+    }
+}
